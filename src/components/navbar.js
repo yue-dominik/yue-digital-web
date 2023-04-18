ecc40cbca38b36de5015ce73a1c99e8ab6672e14
yue-dominik <dominik.pavicic@yuedigital.com>
2023-04-18 18:57:01 +0900

    Add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
+import logo from './logo.svg';
+
 
 export const Navbar =()=>{
       return (
-            <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/nephtyke">Nephtyke</Link>
-                  <Link to="/nephtyke-video">Nephtyke Sample</Link>
+            <div className="navbar">
+                  <Link to="/"><img src={logo} className="logo" alt="logo" /></Link>
+                  <Link to="/nephtyke" className="navbar-link">Nephtyke</Link>
+                  <Link to="/nephtyke-video" className="navbar-link">Video</Link>
             </div>
       )
 }
