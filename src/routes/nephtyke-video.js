b89b97b0b8e8be4d498cd81942976694ad98b4b3
yue-dominik <dominik.pavicic@yuedigital.com>
2023-04-26 20:22:11 +0900

    Use correct videos

--- src/routes/nephtyke-video.js
+++ src/routes/nephtyke-video.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { InfoSection  } from '../components/info-section';
 
-export const NephtykeVideo = () =>{
+export const NephtykeVideo = ({ locale }) =>{
     return <div className='page-content'>
         <InfoSection title="Sample Video">
             <div className='video-wrapper'>
-                <iframe className='video' src="https://www.youtube.com/embed/tgbNymZ7vqY" />
+                <iframe className='video' src={
+                    locale === 'ja' ? 'https://www.youtube.com/embed/of3U7epBX_U' : 'https://www.youtube.com/embed/cPe-BmypocI'
+                } />
             </div>
         </InfoSection>
     </div>
